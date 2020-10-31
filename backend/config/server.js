b0e482051231ad42fdb1a76218caab4c4924c5d6
Nicolas Iniesta <inicolas69@gmail.com>
2020-10-31 18:16:18 +0100

    Front et back créé et build

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f9f5525069d3627eca75bf13d9b344b2'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ba38e8e4-3027-4624-98f5-b177913a6852'
+};
